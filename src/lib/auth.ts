a84a00f03763b9d327ef8905503cb4269976b623
tigawanna <denniskinuthiawaweru@gmail.com>
2025-08-08 23:44:00 +0300

    feat: refactor database connection by moving to database.ts and removing unused drizzle client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./drizzle/client";
+import { db } from "./database";
+
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { serverEnvs } from "../env";
+import { serverEnvs } from "./env";
 import { EnhancedQueryLogger } from "drizzle-query-logger";
 
 export const db = drizzle(serverEnvs.DATABASE_URL, {
