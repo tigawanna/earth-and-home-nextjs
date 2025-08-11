2da1133b0f0418efbb5af10f039d48f6a64336e6
tigawanna <denniskinuthiawaweru@gmail.com>
2025-08-11 20:50:35 +0300

    feat: update DaisyUI theme colors and improve layout component structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { Header } from "@/components/root/Header";
+
 
 
 const geistSans = Geist({
@@ -67,7 +67,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider
+          attribute="data-theme"
+          defaultTheme="system"
+          enableSystem>
           <div>
             <div className="fixed inset-0 bg-gradient-to-b from-background to-background/80 z-[-1]" />
             {children}
